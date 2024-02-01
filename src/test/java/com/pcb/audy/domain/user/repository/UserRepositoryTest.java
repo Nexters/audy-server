e7c0d0d738915a3b46499b627a1f787d27805bab
emost22 <emoney96@naver.com>
2024-02-02 03:38:50 +0900

    [#18] Test: add finduser by oauthId repo

---
+++ src/test/java/com/pcb/audy/domain/user/repository/UserRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.pcb.audy.domain.user.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.pcb.audy.domain.user.entity.User;
+import com.pcb.audy.test.UserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest implements UserTest {
+    @Autowired private UserRepository userRepository;
+
+    @Test
+    @DisplayName("oauthId로 유저 조회 테스트")
+    void oauthId_유저_조회() {
+        // given
+        User savedUser = userRepository.save(TEST_USER);
+
+        // when
+        User user = userRepository.findByOauthId(TEST_OAUTH_ID);
+
+        // then
+        assertThat(user).isEqualTo(savedUser);
+    }
+}
