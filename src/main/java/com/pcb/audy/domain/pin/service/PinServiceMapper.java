0f3baa9f792c684262a947f2b969c4fd63de3b2f
emost22 <emoney96@naver.com>
2024-02-06 02:12:58 +0900

    [#33] Fix: delete mapping

--- src/main/java/com/pcb/audy/domain/pin/service/PinServiceMapper.java
+++ src/main/java/com/pcb/audy/domain/pin/service/PinServiceMapper.java
@@ -3,13 +3,11 @@ package com.pcb.audy.domain.pin.service;
 import com.pcb.audy.domain.pin.dto.response.PinSaveRes;
 import com.pcb.audy.domain.pin.entity.Pin;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface PinServiceMapper {
     PinServiceMapper INSTANCE = Mappers.getMapper(PinServiceMapper.class);
 
-    @Mapping(source = "pinId", target = "pinId")
     PinSaveRes toPinSaveRes(Pin pin);
 }
