02cb4303a49e579aed3d1e284dd0e7dd0e3bfa1f
emost22 <emoney96@naver.com>
2024-02-05 16:00:21 +0900

    [#27] Test: add update pin name service

--- src/test/java/com/pcb/audy/domain/pin/service/PinServiceTest.java
+++ src/test/java/com/pcb/audy/domain/pin/service/PinServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.pcb.audy.domain.course.repository.CourseRepository;
+import com.pcb.audy.domain.pin.dto.request.PinNameUpdateReq;
 import com.pcb.audy.domain.pin.dto.request.PinSaveReq;
 import com.pcb.audy.global.redis.RedisProvider;
 import com.pcb.audy.test.PinTest;
@@ -44,6 +45,26 @@ class PinServiceTest implements PinTest {
 
         // then
         verify(courseRepository).findByCourseId(any());
+        verify(redisProvider).set(any(), any(), anyLong());
+    }
+
+    @Test
+    @DisplayName("pin 이름 수정 테스트")
+    void pin_이름_수정() {
+        // given
+        PinNameUpdateReq pinNameUpdateReq =
+                PinNameUpdateReq.builder()
+                        .courseId(TEST_COURSE_ID)
+                        .pinId(TEST_PIN_ID)
+                        .pinName(TEST_UPDATED_PIN_NAME)
+                        .build();
+        when(redisProvider.get(any())).thenReturn(TEST_PIN);
+
+        // when
+        pinService.updatePinName(pinNameUpdateReq);
+
+        // then
+        verify(redisProvider).get(any());
         verify(redisProvider).set(any(), any(), anyLong());
     }
 }
