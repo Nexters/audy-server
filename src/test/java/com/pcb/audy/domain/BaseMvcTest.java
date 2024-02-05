4a47705ad0afb7bbe8309380d31f77a7395f1a16
Haeun Kim <gkdms1120s@gmail.com>
2024-02-06 00:39:52 +0900

    [#17] Test: Course 저장 api Test 추가

--- src/test/java/com/pcb/audy/domain/BaseMvcTest.java
+++ src/test/java/com/pcb/audy/domain/BaseMvcTest.java
@@ -1,9 +1,15 @@
 package com.pcb.audy.domain;
 
+import static com.pcb.audy.test.UserTest.TEST_USER;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
 import capital.scalable.restdocs.AutoDocumentation;
 import capital.scalable.restdocs.jackson.JacksonResultHandlers;
 import capital.scalable.restdocs.response.ResponseModifyingPreprocessors;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pcb.audy.global.auth.PrincipalDetails;
+import com.pcb.audy.global.jwt.MockSpringSecurityFilter;
+import java.security.Principal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -14,6 +20,8 @@ import org.springframework.restdocs.cli.CliDocumentation;
 import org.springframework.restdocs.http.HttpDocumentation;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,6 +33,7 @@ public class BaseMvcTest {
 
     @Autowired protected ObjectMapper objectMapper;
     protected MockMvc mockMvc;
+    protected Principal mockPrincipal;
     @Autowired private WebApplicationContext context;
 
     @BeforeEach
@@ -44,6 +53,7 @@ public class BaseMvcTest {
                                                 ResponseModifyingPreprocessors.replaceBinaryContent(),
                                                 ResponseModifyingPreprocessors.limitJsonArrayLength(objectMapper),
                                                 Preprocessors.prettyPrint())))
+                        .apply(springSecurity(new MockSpringSecurityFilter()))
                         .apply(
                                 MockMvcRestDocumentation.documentationConfiguration(restDocumentation)
                                         .uris()
@@ -64,5 +74,14 @@ public class BaseMvcTest {
                                                 AutoDocumentation.methodAndPath(),
                                                 AutoDocumentation.section()))
                         .build();
+        mockUserSetup();
+    }
+
+    private void mockUserSetup() {
+        UserDetails testUserDetails = PrincipalDetails.builder().user(TEST_USER).build();
+
+        mockPrincipal =
+                new UsernamePasswordAuthenticationToken(
+                        testUserDetails, "", testUserDetails.getAuthorities());
     }
 }
---
+++ src/test/java/com/pcb/audy/domain/course/service/CourseServiceTest.java
@@ -0,0 +1,72 @@
+package com.pcb.audy.domain.course.service;
+
+import static com.pcb.audy.global.response.ResultCode.NOT_FOUND_USER;
+import static com.pcb.audy.test.UserTest.TEST_USER;
+import static com.pcb.audy.test.UserTest.TEST_USER_ID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.pcb.audy.domain.course.dto.request.CourseSaveReq;
+import com.pcb.audy.domain.course.entity.Course;
+import com.pcb.audy.domain.course.repository.CourseRepository;
+import com.pcb.audy.domain.user.repository.UserRepository;
+import com.pcb.audy.global.exception.GlobalException;
+import com.pcb.audy.global.meta.Role;
+import com.pcb.audy.test.PinTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CourseServiceTest implements PinTest {
+    @InjectMocks private CourseService courseService;
+    @Mock private CourseRepository courseRepository;
+    @Mock private UserRepository userRepository;
+
+    @Captor ArgumentCaptor<Course> argumentCaptor;
+
+    @Test
+    @DisplayName("course 저장 테스트")
+    void course_저장() {
+        // given
+        CourseSaveReq courseSaveReq =
+                CourseSaveReq.builder().userId(TEST_USER_ID).courseName(TEST_COURSE_NAME).build();
+        when(userRepository.findByUserId(any())).thenReturn(TEST_USER);
+
+        // when
+        courseService.saveCourse(courseSaveReq);
+
+        // then
+        verify(userRepository).findByUserId(any());
+        verify(courseRepository).save(argumentCaptor.capture());
+        assertEquals(TEST_USER, argumentCaptor.getValue().getEditorList().get(0).getUser());
+        assertEquals(Role.OWNER, argumentCaptor.getValue().getEditorList().get(0).getRole());
+    }
+
+    @Test
+    @DisplayName("course 저장 실패 테스트 - 사용자 권한 없음")
+    void invalidUserSaveCourseTest() {
+        // given
+        CourseSaveReq courseSaveReq =
+                CourseSaveReq.builder().userId(TEST_USER_ID).courseName(TEST_COURSE_NAME).build();
+
+        // when
+        GlobalException exception =
+                assertThrows(
+                        GlobalException.class,
+                        () -> {
+                            courseService.saveCourse(courseSaveReq);
+                        });
+
+        // then
+        assertEquals(NOT_FOUND_USER, exception.getResultCode());
+    }
+}
