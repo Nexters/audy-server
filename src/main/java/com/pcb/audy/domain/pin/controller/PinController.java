5d7463d9a24bf703ff3176499c2f3c5fe3c63494
emost22 <emoney96@naver.com>
2024-02-05 15:56:21 +0900

    [#27] Feat: add update pin name api

--- src/main/java/com/pcb/audy/domain/pin/controller/PinController.java
+++ src/main/java/com/pcb/audy/domain/pin/controller/PinController.java
@@ -1,10 +1,13 @@
 package com.pcb.audy.domain.pin.controller;
 
+import com.pcb.audy.domain.pin.dto.request.PinNameUpdateReq;
 import com.pcb.audy.domain.pin.dto.request.PinSaveReq;
+import com.pcb.audy.domain.pin.dto.response.PinNameUpdateRes;
 import com.pcb.audy.domain.pin.dto.response.PinSaveRes;
 import com.pcb.audy.domain.pin.service.PinService;
 import com.pcb.audy.global.response.BasicResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +22,11 @@ public class PinController {
     @PostMapping
     public BasicResponse<PinSaveRes> savePin(@RequestBody PinSaveReq pinSaveReq) {
         return BasicResponse.success(pinService.savePin(pinSaveReq));
+    }
+
+    @PatchMapping
+    public BasicResponse<PinNameUpdateRes> updatePinName(
+            @RequestBody PinNameUpdateReq pinNameUpdateReq) {
+        return BasicResponse.success(pinService.updatePinName(pinNameUpdateReq));
     }
 }
