53e29bee14b877d5dd96082184eec8539ce7a57b
Haeun Kim <gkdms1120s@gmail.com>
2024-02-07 22:53:09 +0900

    [#48] Fix: Course Repository 테스트 오류 수정

--- src/test/java/com/pcb/audy/domain/editor/repository/EditorRepositoryTest.java
+++ src/test/java/com/pcb/audy/domain/editor/repository/EditorRepositoryTest.java
@@ -8,6 +8,7 @@ import com.pcb.audy.domain.user.repository.UserRepository;
 import com.pcb.audy.global.meta.Role;
 import com.pcb.audy.test.CourseTest;
 import com.pcb.audy.test.EditorTest;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,29 +40,41 @@ public class EditorRepositoryTest implements EditorTest {
     @DisplayName("Course와 User로 권한 정보 조회 테스트")
     void course_user_권한_조회() {
         // given
-        userRepository.save(TEST_USER);
-        courseRepository.save(TEST_COURSE);
-        editorRepository.save(TEST_EDITOR_ADMIN);
+        User savedUser = userRepository.save(TEST_USER);
+        Course savedCourse = courseRepository.save(TEST_COURSE);
+        editorRepository.save(Editor.builder()
+                        .user(savedUser)
+                        .course(savedCourse)
+                        .role(Role.OWNER)
+                        .build());
 
         // when
         Editor editor = editorRepository.findByUserAndCourse(TEST_USER, TEST_COURSE);
 
         // then
-        assertEquals(TEST_USER_ID, editor.getUser().getUserId());
-        assertEquals(TEST_COURSE_ID, editor.getCourse().getCourseId());
+        assertEquals(savedUser.getUserId(), editor.getUser().getUserId());
+        assertEquals(savedCourse.getCourseId(), editor.getCourse().getCourseId());
     }
 
     @Test
     @DisplayName("user의 모든 편집 권한 조회 테스트")
     void user_권한_조회() {
         // given
-        userRepository.save(TEST_USER);
+        User savedUser = userRepository.save(TEST_USER);
 
-        courseRepository.save(TEST_COURSE);
-        courseRepository.save(TEST_SECOND_COURSE);
+        Course savedCourse1 = courseRepository.save(TEST_COURSE);
+        Course savedCourse2 = courseRepository.save(TEST_SECOND_COURSE);
 
-        editorRepository.save(TEST_EDITOR_ADMIN);
-        editorRepository.save(TEST_EDITOR_MEMBER);
+        editorRepository.save(Editor.builder()
+                .user(savedUser)
+                .course(savedCourse1)
+                .role(Role.OWNER)
+                .build());
+        editorRepository.save(Editor.builder()
+                .user(savedUser)
+                .course(savedCourse2)
+                .role(Role.MEMBER)
+                .build());
 
         // when
         List<Editor> editorList = editorRepository.findAllByUserOrderByCreateTimestampDesc(TEST_USER);
@@ -74,13 +87,21 @@ public class EditorRepositoryTest implements EditorTest {
     @DisplayName("특정 권한을 가진 유저의 편집 권한 조회 테스트")
     void user_특정권한_조회() {
         // given
-        userRepository.save(TEST_USER);
+        User savedUser = userRepository.save(TEST_USER);
 
-        courseRepository.save(TEST_COURSE);
-        courseRepository.save(TEST_SECOND_COURSE);
+        Course savedCourse1 = courseRepository.save(TEST_COURSE);
+        Course savedCourse2 = courseRepository.save(TEST_SECOND_COURSE);
 
-        editorRepository.save(TEST_EDITOR_ADMIN);
-        editorRepository.save(TEST_EDITOR_MEMBER);
+        editorRepository.save(Editor.builder()
+                .user(savedUser)
+                .course(savedCourse1)
+                .role(Role.OWNER)
+                .build());
+        editorRepository.save(Editor.builder()
+                .user(savedUser)
+                .course(savedCourse2)
+                .role(Role.MEMBER)
+                .build());
 
         // when
         List<Editor> editorList1 = editorRepository.findAllByUserAndRoleOrderByCreateTimestampDesc(TEST_USER, Role.OWNER);
