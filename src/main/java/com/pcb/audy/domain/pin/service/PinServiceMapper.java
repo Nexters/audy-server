392cc87958e2f19786d4f18fa3ed261dc0dea982
emost22 <emoney96@naver.com>
2024-02-15 08:16:09 +0900

    [#68] Fix: save pin logic use dto

--- src/main/java/com/pcb/audy/domain/pin/service/PinServiceMapper.java
+++ src/main/java/com/pcb/audy/domain/pin/service/PinServiceMapper.java
@@ -1,13 +1,18 @@
 package com.pcb.audy.domain.pin.service;
 
+import com.pcb.audy.domain.pin.dto.request.PinSaveReq;
+import com.pcb.audy.domain.pin.dto.response.PinDataRes;
 import com.pcb.audy.domain.pin.dto.response.PinSaveRes;
-import com.pcb.audy.domain.pin.entity.Pin;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface PinServiceMapper {
     PinServiceMapper INSTANCE = Mappers.getMapper(PinServiceMapper.class);
 
-    PinSaveRes toPinSaveRes(Pin pin);
+    PinSaveRes toPinSaveRes(PinDataRes pinDataRes);
+
+    @Mapping(target = "pinId", expression = "java(java.util.UUID.randomUUID())")
+    PinDataRes toPinDataRes(PinSaveReq pinSaveReq);
 }
