118c19547d88cd21756f95dec8723a5bf17bd688
emost22 <emoney96@naver.com>
2024-02-18 14:21:41 +0900

    [#noissue] Fix: add sockjs

--- src/main/java/com/pcb/audy/global/socket/config/WebSocketConfig.java
+++ src/main/java/com/pcb/audy/global/socket/config/WebSocketConfig.java
@@ -16,8 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/course").setAllowedOriginPatterns("*");
-        //                .withSockJS();
+        registry.addEndpoint("/course").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
