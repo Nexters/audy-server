67d6394904050341b2695da0aa1d42c313af46d5
emost22 <emoney96@naver.com>
2024-02-21 14:16:35 +0900

    [#85] Fix: sendTo

--- src/main/java/com/pcb/audy/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pcb/audy/global/exception/GlobalExceptionHandler.java
@@ -1,18 +1,15 @@
 package com.pcb.audy.global.exception;
 
 import com.pcb.audy.global.response.BasicResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-@RequiredArgsConstructor
 public class GlobalExceptionHandler {
-    private final SimpMessagingTemplate simpMessagingTemplate;
 
     @ExceptionHandler(GlobalException.class)
     public BasicResponse<Void> handleException(GlobalException e) {
@@ -20,7 +17,8 @@ public class GlobalExceptionHandler {
     }
 
     @MessageExceptionHandler(GlobalException.class)
-    public void handleSocketException(GlobalException e) {
-        simpMessagingTemplate.convertAndSend("/sub/error", BasicResponse.error(e.getResultCode()));
+    @SendTo("/sub/error")
+    public BasicResponse<Void> handleSocketException(GlobalException e) {
+        return BasicResponse.error(e.getResultCode());
     }
 }
