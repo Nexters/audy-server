6c5c99c58373237e8483a5225d38abd9409e838b
emost22 <emoney96@naver.com>
2024-02-25 02:29:51 +0900

    [#112] Fix: delete socket handler

--- src/main/java/com/pcb/audy/global/socket/config/WebSocketConfig.java
+++ src/main/java/com/pcb/audy/global/socket/config/WebSocketConfig.java
@@ -2,11 +2,9 @@ package com.pcb.audy.global.socket.config;
 
 import com.pcb.audy.global.socket.handler.CustomHandshakeInterceptor;
 import com.pcb.audy.global.socket.handler.SocketErrorHandler;
-import com.pcb.audy.global.socket.handler.SocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 import org.springframework.web.socket.server.HandshakeInterceptor;
@@ -16,7 +14,6 @@ import org.springframework.web.socket.server.HandshakeInterceptor;
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final SocketHandler socketHandler;
     private final SocketErrorHandler socketErrorHandler;
 
     @Override
@@ -33,11 +30,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub"); // 클라이언트에게 메시지 전달
         registry.setApplicationDestinationPrefixes("/pub"); // 클라이언트에서 보낸 메시지 받기
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(socketHandler);
     }
 
     @Bean
