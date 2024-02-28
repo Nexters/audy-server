7333bd3019e5e09f5f73b82ae7d856ae0dba5e3f
emost22 <emoney96@naver.com>
2024-02-28 16:28:22 +0900

    [#108] Feat: add update token socket handshake

--- src/main/java/com/pcb/audy/global/socket/handler/CustomHandshakeInterceptor.java
+++ src/main/java/com/pcb/audy/global/socket/handler/CustomHandshakeInterceptor.java
@@ -8,12 +8,14 @@ import com.pcb.audy.global.jwt.JwtUtils;
 import com.pcb.audy.global.validator.TokenValidator;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 import org.springframework.web.util.WebUtils;
@@ -36,6 +38,7 @@ public class CustomHandshakeInterceptor implements HandshakeInterceptor {
             if (email == null) {
                 email = getEmail(servletRequest, REFRESH_TOKEN_NAME);
                 TokenValidator.validateEmail(email);
+                updateTokens(response, email);
             }
 
             attributes.put("email", email);
@@ -51,6 +54,14 @@ public class CustomHandshakeInterceptor implements HandshakeInterceptor {
             ServerHttpResponse response,
             WebSocketHandler wsHandler,
             Exception exception) {}
+
+    private void updateTokens(ServerHttpResponse response, String email) {
+        if (response instanceof ServletServerHttpResponse servletServerResponse) {
+            HttpServletResponse servletResponse = servletServerResponse.getServletResponse();
+            jwtUtils.setAccessToken(servletResponse, email);
+            jwtUtils.setRefreshToken(servletResponse, email);
+        }
+    }
 
     private String getEmail(HttpServletRequest request, String tokenName) {
         Cookie cookie = WebUtils.getCookie(request, tokenName);
