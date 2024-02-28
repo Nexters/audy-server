266646fd39326602945927c75fae2601e0487dc8
emost22 <emoney96@naver.com>
2024-02-28 16:24:13 +0900

    [#108] Feat: add socket principal

--- src/main/java/com/pcb/audy/global/socket/config/WebSocketConfig.java
+++ src/main/java/com/pcb/audy/global/socket/config/WebSocketConfig.java
@@ -1,14 +1,18 @@
 package com.pcb.audy.global.socket.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pcb.audy.domain.user.repository.UserRepository;
 import com.pcb.audy.global.jwt.JwtUtils;
 import com.pcb.audy.global.redis.RedisProvider;
 import com.pcb.audy.global.socket.handler.CustomHandshakeInterceptor;
+import com.pcb.audy.global.socket.handler.CustomPrincipalHandshakeHandler;
 import com.pcb.audy.global.socket.handler.SocketErrorHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.HandshakeHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 @Configuration
@@ -19,6 +23,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final SocketErrorHandler socketErrorHandler;
     private final RedisProvider redisProvider;
     private final JwtUtils jwtUtils;
+    private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -26,6 +32,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .addEndpoint("/course/{courseId}")
                 .setAllowedOriginPatterns("*")
                 .addInterceptors(customHandshakeInterceptor())
+                .setHandshakeHandler(customPrincipalHandshakeHandler())
                 .withSockJS();
         registry.setErrorHandler(socketErrorHandler);
     }
@@ -34,6 +41,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub"); // 클라이언트에게 메시지 전달
         registry.setApplicationDestinationPrefixes("/pub"); // 클라이언트에서 보낸 메시지 받기
+    }
+
+    @Bean
+    public HandshakeHandler customPrincipalHandshakeHandler() {
+        return new CustomPrincipalHandshakeHandler(userRepository, objectMapper);
     }
 
     @Bean
